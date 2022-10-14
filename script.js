be860912d8495e9a8ad81ea1c11eb16b9ce34c92
alysvolatile <alice.g.cooper@gmail.com>
2022-10-14 10:14:16 -0400

    created timer app

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-const countTo = "January 1, 2023"
+// Time Countdown
+let time; // time in minutes
+
+const countTo = "timer ends"
 const display = document.querySelector("#countdown")
 let countToDisplay = document.querySelector("#count-to")
 countToDisplay.textContent = `Until ${countTo}!`
+let startButton = document.querySelector("#start-timer")
+let promoTimer;
 
-const countdown = setInterval(() => {
-    const endDate = new Date(countTo)
-    const currentDate = new Date();
-    const totalSeconds = (endDate - currentDate) / 1000;
-
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const minutes = Math.floor(totalSeconds / 60) % 60 ;
-    const seconds = Math.floor(totalSeconds) % 60;
-    let displayValue = (`${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`)
-    display.textContent = displayValue;
-
-    if (totalSeconds < 0) {
-        clearInterval(countdown);
-        display.textContent = 'Happy New Year!!!'
+startButton.addEventListener('click', () => {
+    if (promoTimer) {
+        clearInterval(promoTimer);
     }
-}, 1000)
+    time = document.querySelector("#timer").value
+    document.querySelector(".countdown-display").style.display = 'block'
+    display.textContent = `${time} minutes left`
+    startCountdown()
+})
+
+
+function startCountdown() {
+    let promoTime = time * 60 
+    promoTimer = setInterval(() => {
+        if (promoTime <= 0) {
+            clearInterval(promoTimer);
+            display.textContent = `The timer has ended!`
+        } else {
+            promoTime--
+            const days = Math.floor(promoTime / 3600 / 24);
+            const hours = Math.floor(promoTime / 3600) % 24;
+            const minutes = Math.floor(promoTime / 60) % 60;
+            const seconds = Math.floor(promoTime % 60);
+            if (hours < 1) {
+                display.textContent = `${minutes} minutes and ${seconds} seconds left`           
+            } else if (hours < 1 && minutes < 1) {
+                display.textContent = `${seconds} seconds left`           
+            } else {
+                display.textContent = `${hours} hours, ${minutes} minutes, and ${seconds} seconds left`           
+            }
+        }
+    }, 1000)
+}
+
+
+
 
